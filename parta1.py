da740344f9bd13146af286d4e8266c9cbbe17b4c
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2021-03-12 07:37:10 +0000

    EDIT

--- parta1.py
+++ parta1.py
@@ -1,2 +1,14 @@
+#import libraries
 import pandas as pd
 import argparse
+
+#read in desired columns from csv file
+covid = pd.read_csv('owid-covid-data.csv',encoding = 'ISO-8859-1')
+covid=covid.loc[:,['location','date','total_cases','new_cases','total_deaths','new_deaths']]
+
+#convert dates columns to months
+covid['date']= pd.to_datetime(covid['date'])
+covid['date'] = covid["date"].dt.strftime("%m")
+covid.rename(columns = {"date": "month"}, inplace = True)
+
+print(covid)
