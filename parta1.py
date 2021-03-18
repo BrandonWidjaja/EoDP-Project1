7c9aba6feff11b4e7fd94a7c09b7783b8d30f494
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2021-03-18 08:47:49 +0000

    COMPLETE

--- parta1.py
+++ parta1.py
@@ -11,7 +11,8 @@ args = parser.parse_args()
 
 #read in desired columns from csv file
 covid = pd.read_csv('owid-covid-data.csv',encoding = 'ISO-8859-1')
-covid = covid.loc[:,['location','date','total_cases','new_cases','total_deaths','new_deaths']]
+covid = covid.loc[:,['location','date','total_cases','new_cases','total_deaths',
+                     'new_deaths']]
 
 #convert dates from 2020 to months
 covid['date'] = pd.to_datetime(covid['date'])
@@ -21,20 +22,21 @@ covid['date'] = covid["date"].dt.strftime("%m")
 covid.rename(columns = {"date": "month"}, inplace = True)
 
 #group by location and month
-grouped_covid = covid.groupby(['location', 'month'], as_index = False).agg({'total_cases': ['max'],
-                                                                          'new_cases': ['sum'],
-                                                                          'total_deaths' :['max'],
-                                                                          'new_deaths': ['sum']})
+grouped_covid = covid.groupby(['location', 'month'], as_index = False).agg({
+    'total_cases': ['max'], 'new_cases': ['sum'], 'total_deaths' :['max'],
+    'new_deaths': ['sum']})
 #remove headers from .agg
 grouped_covid.columns = grouped_covid.columns.droplevel(1)
 
 #parta1.2
 
 #add fatality rate column
-grouped_covid["case_fatality_rate"] = grouped_covid["total_deaths"].div(grouped_covid["total_cases"])
+grouped_covid["case_fatality_rate"] = grouped_covid["total_deaths"].div(
+    grouped_covid["total_cases"])
 #move it to desired column
-grouped_covid = grouped_covid[['location','month', 'case_fatality_rate','total_cases','new_cases',
-                             'total_deaths','new_deaths']]
+grouped_covid = grouped_covid[['location','month', 'case_fatality_rate',
+                               'total_cases','new_cases', 'total_deaths',
+                               'new_deaths']]
 
 #remove all rows with any NaN values i.e. no case_fatality_rate entry
 #this will be done by replacing all NaN values with an empty string
