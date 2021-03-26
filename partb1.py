e72f47d0b83edf30886cca64394e570c565b8f65
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2021-03-26 08:20:38 +0000

    EDIT

--- partb1.py
+++ partb1.py
@@ -3,3 +3,17 @@
 import re
 import pandas as pd
 import os
+
+directory = 'cricket'
+df = pd.DataFrame(columns = ['Filename', 'Document ID'])
+
+for filename in os.listdir(directory):
+    if filename.endswith(".txt"):
+        
+        continue
+    else:
+        continue
+
+df.to_csv("test.csv", index = False)
+
+#REGEX IS [A-Z]{4}\-\d{3}[A-Z]{0,1}
